770d30e711a16567fbe9c9829d9b294c282a3e21
abackwood <a.backwood@gmail.com>
2017-01-12 14:32:35 +0100

    INitial Program.cs file

---
+++ ConfluenceAPIClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using Newtonsoft.Json;
+
+public class Program {
+    public const string APIEndpoint = "https://ns-confl.dev.infosupport.net/rest/api/";
+
+    public static void Main(string[] args) {
+        string pageTitle = "ConfluenceAPIClient Test Pagina";
+        string pageContent = "<p>Dit is een test pagina aangemaakt door de ConfluenceAPIClient</p>";
+
+        HttpClient client = new HttpClient();
+    }
+}
